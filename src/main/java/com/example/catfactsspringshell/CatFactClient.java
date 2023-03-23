b64d6a0b5d2e4d53e433aa1cdeb2fd4437030a97
gtamaaas <gaspar.tamas315@gmail.com>
2023-03-23 21:00:41 +0200

    Refactored to leverage Spring features, such as @Bean and @Autowired

--- src/main/java/com/example/catfactsspringshell/CatFactClient.java
+++ src/main/java/com/example/catfactsspringshell/CatFactClient.java
@@ -1,15 +1,22 @@
 package com.example.catfactsspringshell;
 
 import com.example.catfactsspringshell.model.CatFact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class CatFactClient {
 
     private String resourceUrl = "https://cat-fact.herokuapp.com/facts/random";
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-    public String getRandomFact() {
-        RestTemplate restTemplate = new RestTemplate();
+    public String getRandomFact(RestTemplate restTemplate) {
         CatFact catFact = restTemplate.getForObject(resourceUrl, CatFact.class);
+        // searches until it finds a suitable cat fact
         while(catFact.getStatus().getVerified() == null) {
             catFact = restTemplate.getForObject(resourceUrl, CatFact.class);
         }
--- src/main/java/com/example/catfactsspringshell/shell/Commands.java
+++ src/main/java/com/example/catfactsspringshell/shell/Commands.java
@@ -1,15 +1,23 @@
 package com.example.catfactsspringshell.shell;
 
 import com.example.catfactsspringshell.CatFactClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.web.client.RestTemplate;
 
 @ShellComponent
 public class Commands {
 
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    CatFactClient catFactClient;
+
     @ShellMethod(value="Gives a random verified cat fact")
     public String cat() {
-        CatFactClient catFactClient = new CatFactClient();
-        return catFactClient.getRandomFact();
+        return catFactClient.getRandomFact(restTemplate);
     }
 }
